c1da0bee5b09d3201cc63ac57090a011c0d08a32
jrjrspnl <jerwinespenile@gmail.com>
2025-05-25 00:16:30 +0800

    remove unnecessary js

--- src/components/aboutPage/Latest.jsx
+++ src/components/aboutPage/Latest.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import wave7 from "../../assets/Waves/Wave7.svg";
-import { LatestProducts } from "./index.js";
+
+import latestGetGitChoco from "../../assets/latestProducts/GetGitChoco.png";
+import latestGetGitCheese from "../../assets/latestProducts/GetGitCheese.png";
+import latestGetGitLemon from "../../assets/latestProducts/GetGitLemon.png";
+import latestChoyoChoyoWaferBites from "../../assets/latestProducts/ChoyoChoyoWaferBites.png";
+import latestSugoSamg from "../../assets/latestProducts/SugoSamg.png";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FreeMode, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "./style.css";
+
+const LatestProducts = [
+  {
+    title: "Get Git Chocolate",
+    product: latestGetGitChoco,
+  },
+  {
+    title: "Get Git Cheese",
+    product: latestGetGitCheese,
+  },
+  {
+    title: "Get Git Lemon",
+    product: latestGetGitLemon,
+  },
+  {
+    title: "Choyo Wafer Bites",
+    product: latestChoyoChoyoWaferBites,
+  },
+  {
+    title: "Sugo Samgyupsal",
+    product: latestSugoSamg,
+  },
+];
 
 const Latest = () => {
   return (
